54a23a3ff3f03a90969aad81ce7ac15e3d833a48
sharik <sharik>
2008-07-17 13:01:13 +0000

    *** empty log message ***

--- gimp-init.el
+++ gimp-init.el
@@ -1,4 +1,27 @@
 ;; This is gimp-init.el
+;;; gimp-init.el --- $Id: gimp-init.el,v 1.15 2008-07-17 13:01:13 sharik Exp $
+;; Copyright (C) 2008 Niels Giesen.
+
+;; Author: Niels Giesen <nielsforkgiesen@gmailspooncom, but please
+;; replace the kitchen utensils with a dot before hitting "Send">
+;; Keywords: processes, multimedia, extensions, tools, gimp, scheme
+;; Homepage: http://niels.kicks-ass.org/gimpmode
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
  ;; Check whether first time
 ;; Put this in your .emacs (load-file "~/.emacs.d/gimp/gimp-init.el")
 (defvar gimp-mode-dir 
